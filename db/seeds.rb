0200c331a50be1bc3a72bc959f40b73c387cb0d9
Chris Cox <chriscox@abandonedfactory.net>
2016-10-07 21:12:15 -0700

    Adding default stations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+stations = WeatherStation.create([
+    {
+        latitude: 24.860971,
+        longitude: -100.413901,
+        name: "Catarino Rodríguez",
+    },
+    {
+        latitude: 45.563092,
+        longitude: -116.971135,
+        name: "Zumwalt Prarie Preserve",
+    }
+])
