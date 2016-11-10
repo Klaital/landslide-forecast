77405eb9d442c62e7a9e74f75e5714812c7786d4
Chris Cox <chris.cox@abandonedfactory.net>
2016-11-10 15:54:29 -0800

    Continuing users implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id]) 
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
