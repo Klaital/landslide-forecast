f67da37db3cdca13eff7b8b1d0ec4ea8ad35d325
Chris Cox <chris.cox@abandonedfactory.net>
2016-11-10 14:50:00 -0800

    Refactoring styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About | AF Landslide Forecasting"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
+    assert_select "title", "Contact Us | AF Landslide Forecasting"
   end
 
 end
