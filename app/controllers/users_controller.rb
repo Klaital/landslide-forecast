97f28f4bcbee63bfd378f0a3ae542017f92de791
Chris Cox <chris.cox@abandonedfactory.net>
2016-11-14 11:33:25 -0800

    Finished signup logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Landslide Forcaster!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
   end
 
   private
